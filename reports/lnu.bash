c470f867135fb21b917920da858c96630b914482
Wayne Arter <wayne.arter@ukaea.uk>
2021-12-24 17:10:50 +0000

    tidy up for Christmas

---
+++ reports/lnu.bash
@@ -0,0 +1,9 @@
+# link to university names
+ln -s  2047352_1   UCL_Coveney
+ln -s  2047352_2   UCL_Guillas
+ln -s  2047353   STFC
+ln -s  2047355   Warwick
+ln -s  2047356   York_and_KCL
+ln -s  2047357   Oxford_Physics
+ln -s  2047358   York_and_Warwick
+ln -s  2048465   KCL_and_Imperial
